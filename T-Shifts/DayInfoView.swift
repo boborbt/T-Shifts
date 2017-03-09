115f1b108b2ace061fc7bdd0c84390409a6eae4b
Roberto Esposito <roberto.esposito@unito.it>
2017-03-09 15:03:02 +0100

    Implemented SecurePanel

--- T-Shifts/DayInfoView.swift
+++ T-Shifts/DayInfoView.swift
@@ -9,22 +9,66 @@
 import UIKit
 
 class DayInfoView: UIView {
+    // MARK: properties
+    
     weak var controller:ViewController!
     var date: Date!
     
     var dayCellView: DayCellView!
     var offscreenCellView: DayCellView!
     var markButtonsArrayView: MarkButtonsArrayView!
+    var securePanelView: UIControl!
     
     var dayCellViewLeadingConstraint: NSLayoutConstraint!
     var offscreenDayCellViewLeadingConstraint: NSLayoutConstraint!
+    var securePanelViewLeadingConstraint: NSLayoutConstraint!
     
     var animateNextTransition: Bool = false
     
+    
+    // MARK: setup
     override func awakeFromNib() {
         addSubviews()
         addAutoLayoutConstraints()
+        
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panelPan(recognizer:)))
+        
+        securePanelView.addGestureRecognizer(gestureRecognizer)
     }
+    
+    func panelPan(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: securePanelView)
+
+        let left_bound = CGFloat(-5)
+        let right_bound = markButtonsArrayView.frame.width - 5
+        
+        securePanelViewLeadingConstraint.constant += translation.x
+        if securePanelViewLeadingConstraint.constant < left_bound {
+            securePanelViewLeadingConstraint.constant = left_bound
+        }
+        
+        if securePanelViewLeadingConstraint.constant > right_bound {
+            securePanelViewLeadingConstraint.constant = right_bound
+        }
+
+        
+        if recognizer.state == .ended {
+            let x = CGFloat(securePanelViewLeadingConstraint.constant) + recognizer.velocity(in: securePanelView).x / 10
+            
+            if abs(x - left_bound) < abs(x - right_bound) {
+                securePanelViewLeadingConstraint.constant = left_bound
+            } else {
+                securePanelViewLeadingConstraint.constant = right_bound
+            }
+            
+            UIView.animate(withDuration: 0.2, animations: { self.layoutIfNeeded() } )
+        } else {
+            recognizer.setTranslation(CGPoint.zero, in: securePanelView)
+            self.layoutIfNeeded()
+        }
+    }
+    
+    // MARK: buttons
     
     func activateMarkButtons(templates:[ShiftTemplate]) {
         for markButton in markButtonsArrayView.markButtonsArray {
@@ -40,20 +84,6 @@ class DayInfoView: UIView {
         setupButtons(controller: controller, templates: templates)
     }
     
-    func show(date newDate: Date, templates: [ShiftTemplate]) {
-        guard self.date != nil else {
-            date = newDate
-            return
-        }
-    
-        dateTransition(to: newDate, templates: templates, animate: animateNextTransition, completion: {
-            self.activateMarkButtons(templates: templates)
-            self.date = newDate
-        })
-        
-        animateNextTransition = false
-    }
-
     
     func setupButtons(controller:ViewController, templates: ShiftTemplates) {
         for markButton in markButtonsArrayView.markButtonsArray {
@@ -67,6 +97,24 @@ class DayInfoView: UIView {
             }
         }
     }
+    
+    // MARK: show date
+    
+    func show(date newDate: Date, templates: [ShiftTemplate]) {
+        guard self.date != nil else {
+            date = newDate
+            return
+        }
+        
+        dateTransition(to: newDate, templates: templates, animate: animateNextTransition, completion: {
+            self.activateMarkButtons(templates: templates)
+            self.date = newDate
+        })
+        
+        animateNextTransition = false
+    }
+
+    // MARK: private members
     
     private func dateTransition(to newDate: Date,
                                 templates: [ShiftTemplate],
@@ -103,6 +151,9 @@ class DayInfoView: UIView {
         cell.marks = templates
     }
     
+
+    
+    
     
     private func addSubviews() {
         dayCellView = DayInfoView.makeDayCellView()
@@ -110,9 +161,13 @@ class DayInfoView: UIView {
         
         markButtonsArrayView = Bundle.main.loadNibNamed("MarkButtonsArrayView", owner: self, options: nil)!.first as! MarkButtonsArrayView
         
+        securePanelView = Bundle.main.loadNibNamed("SecurePanelView", owner: self, options: nil)!.first as! UIControl
+    
+        
         self.addSubview(dayCellView)
         self.addSubview(offscreenCellView)
         self.addSubview(markButtonsArrayView)
+        self.addSubview(securePanelView)
     }
     
     
@@ -155,20 +210,32 @@ class DayInfoView: UIView {
         dayCellViewLeadingConstraint.isActive = true
         dayCellView.widthAnchor.constraint(equalTo: self.heightAnchor, constant: -10).isActive = true
         
+        
         offscreenCellView.translatesAutoresizingMaskIntoConstraints = false
         offscreenCellView.topAnchor.constraint(equalTo: self.topAnchor, constant: +5).isActive = true
         offscreenCellView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5).isActive = true
         
         offscreenDayCellViewLeadingConstraint.isActive = true
         offscreenCellView.widthAnchor.constraint(equalTo: self.heightAnchor, constant: -10).isActive = true
-
         
         
         markButtonsArrayView.translatesAutoresizingMaskIntoConstraints = false
         markButtonsArrayView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: self.frame.size.height + 5).isActive = true
-        markButtonsArrayView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        markButtonsArrayView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        markButtonsArrayView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        markButtonsArrayView.topAnchor.constraint(equalTo: self.topAnchor, constant: +5).isActive = true
+        markButtonsArrayView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5).isActive = true
+        markButtonsArrayView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5).isActive = true
+        
+        
+        securePanelViewLeadingConstraint = securePanelView.leadingAnchor.constraint(equalTo: markButtonsArrayView.leadingAnchor, constant: -5)
+        
+        securePanelView.translatesAutoresizingMaskIntoConstraints = false
+        securePanelView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        securePanelViewLeadingConstraint.isActive = true
+        
+        securePanelView.widthAnchor.constraint(equalTo: markButtonsArrayView.widthAnchor, constant: +10).isActive = true
+
+        securePanelView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
     }
     
     
