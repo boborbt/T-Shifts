2367a44a4ff5bd3f938b8b3b68971ef85690c9cc
boborbt <boborbt@gmail.com>
2019-02-18 16:23:31 +0100

    Moved framework inside main project

---
+++ TShiftsFramework/TShiftsFramework.h
@@ -0,0 +1,19 @@
+//
+//  TShiftsFramework.h
+//  TShiftsFramework
+//
+//  Created by Roberto Esposito on 18/02/2019.
+//  Copyright © 2019 Roberto Esposito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TShiftsFramework.
+FOUNDATION_EXPORT double TShiftsFrameworkVersionNumber;
+
+//! Project version string for TShiftsFramework.
+FOUNDATION_EXPORT const unsigned char TShiftsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TShiftsFramework/PublicHeader.h>
+
+
