32e75cfc333389f5f37bd59449a283b748285308
Roberto Esposito <roberto.esposito@unito.it>
2019-05-25 19:03:56 +0200

    Finished UI for alert settings

--- T-Shifts/EditTemplateViewController.swift
+++ T-Shifts/EditTemplateViewController.swift
@@ -12,6 +12,7 @@ import TShiftsFramework
 
 class EditTemplateViewController: UIViewController {
     
+    // MARK: OUTLETS
     
     @IBOutlet weak var shiftDescription: UILabel!
     @IBOutlet weak var timePickersView: UIView!
@@ -28,6 +29,8 @@ class EditTemplateViewController: UIViewController {
     
     @IBOutlet weak var alertSwitch: UISwitch!
     @IBOutlet weak var alertMinutes: UITextField!
+    
+    // MARK: PROPERTIES
     
     var updateShiftCallback: (() -> ())!
     var hourFormatter: DateFormatter!
@@ -82,8 +85,8 @@ class EditTemplateViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        startDatePickerSetVisibility(hidden: true, animated: false)
-        endDatePickerSetVisibility(hidden: true, animated: false)
+        datePickerSetVisibility(startHourPicker, hidden: true, animated: false)
+        datePickerSetVisibility(endHourPicker, hidden: true, animated: false)
         
         hourFormatter = DateFormatter()
         hourFormatter.dateStyle = .none
@@ -93,13 +96,16 @@ class EditTemplateViewController: UIViewController {
         
         // add a done button to the numberpad
         keypadToolbar.items=[
-            UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: alertMinutes, action: #selector(UITextField.resignFirstResponder)),
-            UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil),
+            UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: alertMinutes, action: #selector(UITextField.resignFirstResponder))
         ]
         keypadToolbar.sizeToFit()
         alertMinutes.inputAccessoryView = keypadToolbar
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        updateShiftCallback()
+    }
     
     // Updates the UI to reflect that the all-day switch changed value
     @IBAction func allDayChanged(_ sender: UISwitch) {
@@ -108,23 +114,34 @@ class EditTemplateViewController: UIViewController {
         }, completion: nil)
     }
     
+    // MARK: ACTIONS
     
     @IBAction func fromLabelTap(_ sender: UITapGestureRecognizer) {
-        os_log(.debug, "from label tap")
-        
         let newState = !startHourPicker.isHidden;
-        startDatePickerSetVisibility(hidden: newState, animated: true)
+        
+        fieldTap(sender)
+        datePickerSetVisibility(startHourPicker, hidden: newState, animated: true)
     }
     
     @IBAction func toLabelTap(_ sender: UITapGestureRecognizer) {
         os_log(.debug, "to label tap")
         let newState = !endHourPicker.isHidden;
-        endDatePickerSetVisibility(hidden: newState, animated: true)
+        
+        fieldTap(sender)
+        datePickerSetVisibility(endHourPicker, hidden: newState, animated: true)
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        updateShiftCallback()
+    
+    @IBAction func fieldTap(_ sender: Any) {
+        datePickerSetVisibility(startHourPicker, hidden: true, animated: true)
+        datePickerSetVisibility(endHourPicker, hidden: true, animated: true)
+        
+        if sender as? UITextField != alertMinutes {
+            os_log(.debug, "Resigning first responder")
+            alertMinutes.resignFirstResponder()
+        }
     }
+    // MARK: PICKERS
     
     @IBAction func startHourPickerDidChange(_ sender: UIDatePicker) {
         startHourLabel.text = hourFormatter.string(from: startHourPicker.date)
@@ -134,25 +151,18 @@ class EditTemplateViewController: UIViewController {
         endHourLabel.text = hourFormatter.string(from: endHourPicker.date)
     }
     
-    func startDatePickerSetVisibility(hidden: Bool, animated: Bool) {
-            startHourPicker.isHidden = hidden
+    func datePickerSetVisibility(_ picker: UIDatePicker, hidden: Bool, animated: Bool) {
+        guard picker.isHidden != hidden else { return }
+        picker.isHidden = hidden
+        
+        let heightConstraint = (picker == self.startHourPicker ? startHourPickerHeightConstraint : endHourPickerHeightConstraint)
         
         UIView.animate(withDuration: (animated ? 0.5 : 0.0) , animations: {
-            self.startHourPickerHeightConstraint.constant = (hidden ? 0 : 242)
+            heightConstraint!.constant = (hidden ? 0 : 242)
             self.view.layoutIfNeeded()
         })
     }
     
-    func endDatePickerSetVisibility(hidden: Bool, animated: Bool) {
-        endHourPicker.isHidden = hidden
-
-        UIView.animate(withDuration: (animated ? 0.5 : 0.0) , animations: {
-            self.endHourPickerHeightConstraint.constant = (hidden ? 0 : 242)
-            self.view.layoutIfNeeded()
-        })
-        
-    }
-
 
     /*
     // MARK: - Navigation
